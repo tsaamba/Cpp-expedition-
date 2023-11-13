132b49463246edee6ca84cf24b712ba04c9bbbbf
tsaamba <146667565+tsaamba@users.noreply.github.com>
2023-11-14 00:38:52 +0530

    Add files via upload

---
+++ Sum of two numbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+  float num1, num2;
+  cout<<"Enter first number: ";
+  cin>>num1;
+  cout<<"Enter second number: ";
+  cin>>num2;
+  cout<<"Sum of two numbers is :" <<num1+num2;
+}
