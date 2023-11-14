ddc0691db8a2e5acd7a76c4d80983db512b98190
tsaamba <146667565+tsaamba@users.noreply.github.com>
2023-11-14 12:58:01 +0530

    Add files via upload

---
+++ Multiplication through user input.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+float a,b;
+cout<<"Enter two decimals"<<endl;
+cin>>a>>b;
+cout<<"Multiplication of above two numbers is: " <<a <<"*"<< b<<" = "<<a*b;
+
+return 0;
+}
