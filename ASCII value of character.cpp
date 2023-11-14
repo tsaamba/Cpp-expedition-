1a3369789dbea416ad0afad7eed263edc7e97751
tsaamba <146667565+tsaamba@users.noreply.github.com>
2023-11-14 15:58:32 +0530

    Add files via upload

---
+++ ASCII value of character.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+char a;
+cout<<"Enter a letter: ";
+cin>> a;
+cout<<"The ASCII value of the letter "<< a << " is " << int(a);
+
+return 0;
+}
