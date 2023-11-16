64d73661fb85f70f97e77a9e700d21f02e6e4b88
tsaamba <146667565+tsaamba@users.noreply.github.com>
2023-11-16 17:16:29 +0530

    Add files via upload

---
+++ Table of any number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+int n;
+cout << "Enter a number of which you want the table to be printed :  " ;
+cin>>n;
+for(int i =1; i<=10; i++){
+cout << n <<"*"<< i << "="<< n*i<< endl;
+}
+return 0;
+}
